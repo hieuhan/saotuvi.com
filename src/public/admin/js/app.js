5ed8ff4e179d7e542660db402e004c9d931bccd6
hieuht <hantrunghieu@gmail.com>
2023-03-20 22:35:14 +0700

    ...

--- src/public/admin/js/app.js
+++ src/public/admin/js/app.js
@@ -44,7 +44,67 @@ var app = {
 
             event.preventDefault();
         });
-    }
+    },
+    fetchData: function (options) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                cache: options.cache || true,
+                url: options.url,
+                type: options.type || 'GET',
+                data: options.data || {},
+                dataType: options.dataType || 'json',
+                beforeSend: options.beforeSend || function(){},
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (error) {
+                    reject(error);
+                }
+            }).always(options.always || function(){});
+        });
+    },
+    bindTableData: function (path, destination) {
+        destination = destination || 'table-result';
+        app.fetchData(
+            {
+                url: path + location.search,
+                dataType: 'html',
+                type: 'post'
+            })
+            .then((response) => {
+                $(`#${destination}`).html(response);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    },
+    addParam: function (currentUrl, key, val) {
+        var url = new URL(currentUrl);
+        url.searchParams.set(key, val);
+        return url.href; 
+    },
+    setQueryParameter: function (uri, key, value) {
+        var re = new RegExp("([?&])(" + key + "=)[^&#]*", "g");
+        if (uri.match(re))
+            return uri.replace(re, '$1$2' + value);
+
+        // need to add parameter to URI
+        var paramString = (uri.indexOf('?') < 0 ? "?" : "&") + key + "=" + value;
+        var hashIndex = uri.indexOf('#');
+        if (hashIndex < 0)
+            return uri + paramString;
+        else
+            return uri.substring(0, hashIndex) + paramString + uri.substring(hashIndex);
+    },
+    pushState: function (object) {
+        if (window.history && window.history.pushState) {
+            var url = new URL(window.location.href);
+            for (var key in object) {
+                url.searchParams.set(key, object[key]);
+            }
+            window.history.pushState(null, '', url);
+        }
+    },
 }
 
 $(function () {
